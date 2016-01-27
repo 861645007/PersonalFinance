56c4e3088f3868a0faf18797fd5eea16c6fabb23
ziye <861645007@qq.com>
2016-01-27 20:33:04 +0800

    Initial Commit

---
+++ PersonalFinance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalFinance
+//
+//  Created by ziye on 16/1/27.
+//  Copyright © 2016年 王焕强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
