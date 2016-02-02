b51445aff4941fac8e093e49fa5ba45fa3e0f320
ziye <861645007@qq.com>
2016-02-02 20:15:43 +0800

    新建一个 “新增消费视图”

---
+++ PersonalFinance/ViewModel/AddNewCustomViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AddNewCustomViewModel.swift
+//  PersonalFinance
+//
+//  Created by ziye on 16/2/2.
+//  Copyright © 2016年 王焕强. All rights reserved.
+//
+
+import Foundation
---
+++ PersonalFinance/ViewModel/MainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewModel.swift
+//  PersonalFinance
+//
+//  Created by ziye on 16/1/31.
+//  Copyright © 2016年 王焕强. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewModel: NSObject {
+
+}
