d17b559f240b01dbad95ddba5404e0b8cc64ceb7
Huangqiang <861645007@qq.com>
2017-02-14 14:59:14 +0800

    新增 点击cell会有水波纹效果

--- PersonalFinance/DayConsumeTableViewCell.swift
+++ PersonalFinance/DayConsumeTableViewCell.swift
@@ -29,18 +29,14 @@ class DayConsumeTableViewCell: UITableViewCell {
         // Configure the view for the selected state
         if selected && lastSelectedState == false {
             self.selectedFlagView.isHidden = false
-            self.cellisSelectedAnimation()
-            
-            self.backgroundColor = UIColor(red:170/255.0, green:138/255.0, blue:179/255.0, alpha:0.35)
+            self.cellSelectedAnimation()
             
             lastSelectedState = true
         }else {
-            
             if lastSelectedState {
-                self.cellisCanceledAnimation()
+                self.cellCanceledAnimation()
             }
             self.backgroundColor = UIColor.clear
-            
             lastSelectedState = false
         }
     }
@@ -51,22 +47,21 @@ class DayConsumeTableViewCell: UITableViewCell {
         self.categoryIcon.image = UIImage(data: (consume.consumeCategory?.iconData)! as Data)
         
         // 将选中指示调颜色 改为图片主要颜色
-        
         self.categoryIcon.image?.getColors(completionHandler: {[unowned self]  (colors) in
             self.selectedFlagView.backgroundColor = colors.primaryColor
         })
     }
     
-    func cellisSelectedAnimation() {
+    func cellSelectedAnimation() {
         self.selectedFlagView.transform = CGAffineTransform(scaleX: 1.0, y: 0.01)
         
-        UIView.animate(withDuration: 1.5, delay: 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [.curveEaseIn], animations: { [unowned self] in
+        UIView.animate(withDuration: 1.5, delay: 0.1, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.0, options: [.curveEaseIn], animations: { [unowned self] in
             self.selectedFlagView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
             
             }, completion: nil)
     }
     
-    func cellisCanceledAnimation() {
+    func cellCanceledAnimation() {
         self.selectedFlagView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
         
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.75, initialSpringVelocity: 0.0, options: [.curveEaseIn], animations: { [unowned self] in
@@ -76,5 +71,38 @@ class DayConsumeTableViewCell: UITableViewCell {
             self.selectedFlagView.isHidden = true
         }
     }
+    
+    // 增加水波纹效果
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        if lastSelectedState {
+            return
+        }
+        
+        let touchLocation: CGPoint = (touches.first?.location(in: self))!
+        let wavesView = createWavesView(location: touchLocation)
+        
+        self.insertSubview(wavesView, at: 0)
+        wavesView.center = touchLocation
+        wavesView.transform = CGAffineTransform(scaleX: 0.05, y: 0.05)
 
+        UIView.animate(withDuration: 0.5, animations: {
+            wavesView.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }) { (finished) in
+            self.backgroundColor = UIColor(red:170/255.0, green:138/255.0, blue:179/255.0, alpha:0.35)
+            wavesView.removeFromSuperview()
+        }
+        
+    }
+    
+    func createWavesView(location: CGPoint) -> UIView {
+        let width = self.frame.size.width * 2
+        let wavesView = UIView(frame: CGRect(origin: location, size: CGSize(width: width, height: width)))
+        
+        wavesView.alpha = 0.8
+        wavesView.backgroundColor = UIColor(red:170/255.0, green:138/255.0, blue:179/255.0, alpha:0.35)
+        
+        return wavesView
+    }
 }
